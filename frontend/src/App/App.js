60f92db35a4044f9253e10d2cbb515918e209e51
Andrew Khrystoiev <andrehrist1991@gmail.com>
2023-01-25 18:58:01 +0200

    Add CRA

---
+++ frontend/src/App/App.js
@@ -0,0 +1,14 @@
+// Modules
+
+// Components
+import Header from '../Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
